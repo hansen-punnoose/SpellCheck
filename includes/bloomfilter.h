3f0e4318afa862ad0c7184ee73998dca444d30db
hansen-punnoose <73456945+hansen-punnoose@users.noreply.github.com>
2023-03-13 20:11:09 -0500

    declared the BloomFilter class

---
+++ includes/bloomfilter.h
@@ -0,0 +1,19 @@
+#ifndef SPELLCHECKER_BLOOM_FILTER_H
+#define SPELLCHECKER_BLOOM_FILTER_H
+
+#include <vector>  // include the <vector> header file
+
+class BloomFilter {
+public:
+    BloomFilter(int size, int numHashes);
+    void addWord(std::string word);
+    bool isWord(std::string word) const;
+private:
+    std::vector<bool> m_bits;
+    int m_size;
+    int m_numHashes;
+    std::vector<int> m_hashes;
+    std::hash<std::string> m_hasher;
+};
+
+#endif // SPELLCHECKER_BLOOM_FILTER_H
